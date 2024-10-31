8f036deef585a182bbe2a1e68df10e64ac86583d
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-10-31 23:12:45 +0800

    10-31-2024 3rd changes

--- schedule_form/urls.py
+++ schedule_form/urls.py
@@ -3,6 +3,6 @@ from .views import schedule, success, pickup_list
 
 urlpatterns = [
     path('<int:pet_id>/', schedule, name='schedule'),
-    path('success/', success, name='success'),  # Add this line
+    path('success/', success, name='success'),  
     path('pickup_list/', pickup_list, name='pickup_list'),
 ]
